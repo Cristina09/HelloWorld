a623484d3321fbded0dacda0928af323dd06a8b8
Cristina <cris.burghelea@yahoo.ro>
2016-06-09 12:49:40 +0300

    first commit
    commit changes to local repo

---
+++ src/HelloWorld.java
@@ -0,0 +1,30 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Cristina on 6/9/2016.
+ */
+
+public class HelloWorld extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1> Hello Kitty!! </h1>");
+    }
+
+    public void destroy()
+    {
+        // do nothing.
+    }
+
+}
